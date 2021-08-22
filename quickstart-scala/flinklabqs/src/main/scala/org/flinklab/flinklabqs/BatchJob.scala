73a92a47c9eb30479c30e71c5535c020e10b35f9
Gordon McCreight <gordon@mccreight.com>
2021-08-22 11:10:51 -0700

    New defaults

--- quickstart-scala/flinklabqs/src/main/scala/org/flinklab/flinklabqs/BatchJob.scala
+++ quickstart-scala/flinklabqs/src/main/scala/org/flinklab/flinklabqs/BatchJob.scala
@@ -16,51 +16,51 @@
  * limitations under the License.
  */
 
-package org.flinklab.flinklabqs;
+package org.flinklab.flinklabqs
 
-import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.scala._
 
 /**
  * Skeleton for a Flink Batch Job.
  *
- * <p>For a tutorial how to write a Flink batch application, check the
+ * For a tutorial how to write a Flink batch application, check the
  * tutorials and examples on the <a href="https://flink.apache.org/docs/stable/">Flink Website</a>.
  *
- * <p>To package your application into a JAR file for execution,
+ * To package your application into a JAR file for execution,
  * change the main class in the POM.xml file to this class (simply search for 'mainClass')
  * and run 'mvn clean package' on the command line.
  */
-public class BatchJob {
+object BatchJob {
 
-	public static void main(String[] args) throws Exception {
-		// set up the batch execution environment
-		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+  def main(args: Array[String]) {
+    // set up the batch execution environment
+    val env = ExecutionEnvironment.getExecutionEnvironment
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataSet<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * https://flink.apache.org/docs/latest/apis/batch/index.html
-		 *
-		 * and the examples
-		 *
-		 * https://flink.apache.org/docs/latest/apis/batch/examples.html
-		 *
-		 */
+    /*
+     * Here, you can start creating your execution plan for Flink.
+     *
+     * Start with getting some data from the environment, like
+     *  env.readTextFile(textPath);
+     *
+     * then, transform the resulting DataSet[String] using operations
+     * like
+     *   .filter()
+     *   .flatMap()
+     *   .join()
+     *   .group()
+     *
+     * and many more.
+     * Have a look at the programming guide:
+     *
+     * https://flink.apache.org/docs/latest/apis/batch/index.html
+     *
+     * and the examples
+     *
+     * https://flink.apache.org/docs/latest/apis/batch/examples.html
+     *
+     */
 
-		// execute program
-		env.execute("Flink Batch Java API Skeleton");
-	}
+    // execute program
+    env.execute("Flink Batch Scala API Skeleton")
+  }
 }
