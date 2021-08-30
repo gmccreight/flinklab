ea5389fbc728a95ddb3339e5f841ea3f425fb010
Gordon McCreight <gordon@mccreight.com>
2021-08-29 21:33:21 -0700

    WIP

--- app/example-wordcount-scala-with-unit-test/000_all.sh
+++ app/example-wordcount-scala-with-unit-test/000_all.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-./020_compile_jar.sh
+# ./020_compile_jar.sh
 ./025_run_unit_tests.sh
-./030_run_jar_in_flink.sh
-./040_open_web_browser_to_dashboard.sh
+# ./030_run_jar_in_flink.sh
+# ./040_open_web_browser_to_dashboard.sh
