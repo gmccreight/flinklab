51ceb323a3949a6334f4fd59986f24b5efa521f9
Alex Charlton <alex.n.charlton@gmail.com>
2021-09-04 15:04:02 -0700

    Add to .gitignore for Metals (and apply a scalafmt)

--- app/example-wordcount-scala-sbt/src/main/scala/org/flinklab/WordCount.scala
+++ app/example-wordcount-scala-sbt/src/main/scala/org/flinklab/WordCount.scala
@@ -6,14 +6,19 @@ object WordCount {
   def main(args: Array[String]): Unit = {
 
     // set up the execution environment
+
     val env = ExecutionEnvironment.getExecutionEnvironment
 
     // get input data
-    val text = env.fromElements("To be, or not to be,--that is the question:--",
-      "Whether 'tis nobler in the mind to suffer", "The slings and arrows of outrageous fortune",
-      "Or to take arms against a sea of troubles,")
+    val text = env.fromElements(
+      "To be, or not to be,--that is the question:--",
+      "Whether 'tis nobler in the mind to suffer",
+      "The slings and arrows of outrageous fortune",
+      "Or to take arms against a sea of troubles,"
+    )
 
-    val counts = text.flatMap { _.toLowerCase.split("\\W+") }
+    val counts = text
+      .flatMap { _.toLowerCase.split("\\W+") }
       .map { (_, 1) }
       .groupBy(0)
       .sum(1)
