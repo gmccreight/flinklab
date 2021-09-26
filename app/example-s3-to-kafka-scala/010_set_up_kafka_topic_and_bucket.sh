aefea88ce7e3073748849a0826e8cb8eba3b211e
Gordon McCreight <gordon@mccreight.com>
2021-09-26 09:51:22 -0700

    Just some documentation updates (formatting, a bit more detail)

--- app/example-s3-to-kafka-scala/010_set_up_kafka_topic_and_bucket.sh
+++ app/example-s3-to-kafka-scala/010_set_up_kafka_topic_and_bucket.sh
@@ -9,5 +9,13 @@ echo "Creating topic"
 $(git rev-parse --show-toplevel)/bin/kafka topics --delete --topic example-out --if-exists
 $(git rev-parse --show-toplevel)/bin/kafka topics --create --topic example-out
 
+
 echo "Syncing data bucket"
+# This data is on the local filesystem at .minio-data/data, which is mounted as
+# /data in the minio container.
+#
+# The command `mc sync` is special in the same way that `mc sh` is.
+# It does not run an mc command, but rather it runs this:
+# `docker-compose run --rm console mc mirror --overwrite --remove /data local/data`
+# which takes the data that is in the /data directory and actually loads it into the 'data' bucket
 $(git rev-parse --show-toplevel)/bin/mc sync
